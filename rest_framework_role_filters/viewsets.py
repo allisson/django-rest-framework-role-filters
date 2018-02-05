5eb256f76b130a40f2c597c072ecdbd23554516d
Allisson Azevedo <allisson@gmail.com>
2018-02-05 10:19:52 -0300

    Make super call compatible with python 2

--- rest_framework_role_filters/viewsets.py
+++ rest_framework_role_filters/viewsets.py
@@ -8,7 +8,7 @@ class RoleFilterModelViewSet(ModelViewSet):
         pass
 
     def initial(self, request, *args, **kwargs):
-        super().initial(request, *args, **kwargs)
+        super(RoleFilterModelViewSet, self).initial(request, *args, **kwargs)
         self.role_id = self.get_role_id(request)
         allowed_actions = self.role_filter_group.get_allowed_actions(self.role_id, request, self)
         if self.action not in allowed_actions:
@@ -18,5 +18,5 @@ class RoleFilterModelViewSet(ModelViewSet):
             )
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        queryset = super(RoleFilterModelViewSet, self).get_queryset()
         return self.role_filter_group.get_queryset(self.role_id, self.request, self, queryset)
