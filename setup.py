39a933600ba87180768589cf792e0e3e4b3b1441
Allisson Azevedo <allisson@gmail.com>
2020-04-18 11:01:10 -0300

    Set min version for drf

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with codecs.open(os.path.join(here, 'CHANGES.rst'), encoding='utf-8') as f:
 
 
 install_requirements = [
-    'djangorestframework>=3.9.4',
+    'djangorestframework>=3.10.3',
 ]
 tests_requirements = [
     'django',
