3b964504141162e66a58184bb3e246aeda92fe4c
Allisson Azevedo <allisson@gmail.com>
2023-10-06 10:00:57 -0300

    ci: set TEST_REQUEST_RENDERER_CLASSES to json only

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -87,4 +87,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
+    "TEST_REQUEST_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
 }
